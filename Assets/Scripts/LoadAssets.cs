0a2b68a8fbd02581101528f2ff0a0e97d4bd136d
Dablestar <jbynchoi@gmail.com>
2024-08-26 16:36:56 +1000

    70 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
-
-    [SerializeField] GameObject blueObj;
+    [SerializeField] private GameObject blueObj;
+    
     // Start is called before the first frame update
     void Start()
     {
