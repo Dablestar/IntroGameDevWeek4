ff7d31c9cfc8d2b573131bb4d55543ae020c5d10
Dablestar <jbynchoi@gmail.com>
2024-08-26 17:11:25 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -1,10 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PrintAndHide : MonoBehaviour
 {
-    private int i;
+    private int i, targetNum;
 
     private string objName;
 
@@ -13,12 +15,20 @@ public class PrintAndHide : MonoBehaviour
     void Start()
     {
         i = 3;
+        targetNum = 150 + (int)(Random.value * 100); 
         objName = this.gameObject.name;
     }
 
     // Update is called once per frame
     void Update()
     {  
-       Debug.Log(objName + ":" + ++i); 
+       Debug.Log(objName + ":" + ++i);
+       if (this.gameObject.tag.Equals("Red") && i == 100)
+       {
+           Destroy(this);
+       }else if (this.gameObject.tag.Equals("Blue") && i == targetNum)
+       {
+           Destroy(this);
+       }
     }
 }
