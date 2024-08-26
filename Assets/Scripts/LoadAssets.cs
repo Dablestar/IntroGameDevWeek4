d3c71df9e76dfeffe2aab73796cf3487d41e3bff
Dablestar <jbynchoi@gmail.com>
2024-08-26 16:31:31 +1000

    70 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
-    
+
+    [SerializeField] GameObject blueObj;
     // Start is called before the first frame update
     void Start()
     {
+        redObj.transform.position = new Vector3(2, 0, 0);
+        GameObject.Instantiate(redObj);
+
+        blueObj.transform.position = new Vector3(-2, 0, 0);
+        GameObject.Instantiate(blueObj);
         
+        QualitySettings.vSyncCount = 0; // Set vSyncCount to 0 so that using .targetFrameRate is enabled.
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
